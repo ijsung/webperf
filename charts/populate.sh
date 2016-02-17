7c3b4c48e1d3710a55ff8bdcb1b240d6addca749
I-Jui Sung <ray@multicorewareinc.com>
2016-02-17 17:33:15 -0600

    Using m4 for the template

--- charts/populate.sh
+++ charts/populate.sh
@@ -1,4 +1,4 @@
-for b in bullet mibench parboil parsec sdvbs spec2000 spec2006
+for b in all bullet mibench parboil parsec sdvbs spec2000 spec2006
 do
-  sed s/templatesuitename/$b/ template.htm > $b.htm
+  gm4 -Dtemplatesuitename=$b template.htm > $b.htm
 done
